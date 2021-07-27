90bf847f4f05a9c231fc51a11b0fe802037f6cd5
matthias_ostertag@web.de <KBo#6XDuNtHBWde>
2021-07-27 22:22:13 +0200

    1

---
+++ esphome/components/OPT3001/OPT3001.cpp
@@ -0,0 +1,10 @@
+#include "OPT3001.h""
+#include "esphome/core/log.h"
+
+namespace esphome {
+namespace sht3xd {
+
+static const char *const TAG = "opt3001";
+
+}// Namespace ESP
+}// Namespace OPT3001
