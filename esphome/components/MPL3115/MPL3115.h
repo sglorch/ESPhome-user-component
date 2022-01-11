7c84b47c024d0056719d3ae9151e23314ee8949d
matthias_ostertag@web.de <KBo#6XDuNtHBWde>
2022-01-11 23:04:30 +0100

    1

--- esphome/components/MPL3115/MPL3115.h
+++ esphome/components/MPL3115/MPL3115.h
@@ -10,7 +10,7 @@ namespace mpl3115 {
 class MPL3115Component : public PollingComponent, public i2c::I2CDevice{
  public:
   void set_pressure_sensor(sensor::Sensor *mpl3115_pressure_sensor) { mpl3115_pressure_sensor_ = mpl3115_pressure_sensor; }
-  void set_temperture_sensor(sensor::Sensor *mpl3115_temperature_sensor) { mpl3115_temperature_sensor_ = mpl3115_temperature_sensor; }
+  void set_temperature_sensor(sensor::Sensor *mpl3115_temperature_sensor) { mpl3115_temperature_sensor_ = mpl3115_temperature_sensor; }
   void set_altitiude_light_sensor(sensor::Sensor *mpl3115_altitude_sensor) { mpl3115_altitude_sensor_ = mpl3115_altitude_sensor; }
 
   void setup() override;
